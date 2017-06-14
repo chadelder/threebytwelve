88f51903287a56f92798597ad65c14c5ca9da891
chadelder <c.j.elder@outlook.com>
2017-06-13 20:15:09 -0400

    database

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
   //'database': 'mongodb://localhost:27017/threebytwelve'
-  'database': 'mongodb:chud:chud@ds125262.mlab.com:25262/threebytwelve'
+  'database': 'mongodb:chud:jerome@ds125262.mlab.com:25262/threebytwelve'
 };
